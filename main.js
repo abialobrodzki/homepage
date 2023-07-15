8ec2e3948832c65cb2b0278e3dd1f0ba8016c95b
Adrian Białobrodzki <adrian.bialobrodzki@gmail.com>
2023-07-15 23:58:53 +0200

    Learning CSS & JS

---
+++ main.js
@@ -0,0 +1,18 @@
+// dodanie zmiennych
+const firstName = 'Adrian';
+const age = 31; // liczby nie wymagają apostrofów
+
+// kroki w kodzie wykonowane po kolei - z góry do dołu
+// przykładowe wywołanie funkcji "alert" - wyświetlenie okna alertu przy załadowaniu strony
+alert('WTF! - znacznik');
+alert("WTF! - cudzysłów"); //można używać podwójnych apostrofów
+
+// przykładowe wywołanie funkcji "console.log" - komunikat wyświetlany w zakładce Konsola (DevTools)
+console.log('Siema!');
+console.log('ok');
+// wywołanie funkcji z użyciem zmiennych
+console.log(firstName);
+console.log(age);
+
+// tworzenie dłuższych ciągów znaków z użyciem ` (patrz tylda)
+console.log(`Nazywam się ${firstName} i mam ${age} lat`);
