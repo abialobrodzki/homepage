d8bfeeb6bb8dcc584b26ab44acaadf51d438646a
Adrian Białobrodzki <adrian.bialobrodzki@gmail.com>
2023-07-16 18:12:53 +0200

    Adding homework - part 3

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+alert('Hola, wędrowcze! Przed wyruszeniem w drogę należy zebrać drużynę.');
+
+//NOTATKI
 // dodanie zmiennych
 const firstName = 'Adrian';
 const age = 31; // liczby nie wymagają apostrofów
 
 // kroki w kodzie wykonowane po kolei - z góry do dołu
 // przykładowe wywołanie funkcji "alert" - wyświetlenie okna alertu przy załadowaniu strony
-alert('WTF! - znacznik');
-alert("WTF! - cudzysłów"); //można używać podwójnych apostrofów
+// alert('WTF! - znacznik');
+// alert("WTF! - cudzysłów"); //można używać podwójnych apostrofów
 
 // przykładowe wywołanie funkcji "console.log" - komunikat wyświetlany w zakładce Konsola (DevTools)
 console.log('Siema!');
-console.log('ok');
+// console.log('ok');
 // wywołanie funkcji z użyciem zmiennych
-console.log(firstName);
-console.log(age);
+// console.log(firstName);
+// console.log(age);
 
 // tworzenie dłuższych ciągów znaków z użyciem ` (patrz tylda)
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
